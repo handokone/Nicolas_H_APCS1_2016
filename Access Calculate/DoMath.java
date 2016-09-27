6efa3ee819ab813b9af01bd6596d66e9be55310c
handokone <18handokon72@millshighsch.org>
2016-09-27 09:32:17 -0700

    Calculate Library

---
+++ Access Calculate/DoMath.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @author APCS1_Nicolas_handoko
+ *
+ */
+public class DoMath {
+	public static void main(String[] args){
+		Calculate.square(12);
+		System.out.println(Calculate.square(12));
+	}
+
+}
